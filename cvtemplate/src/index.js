21ca7e62bdaf2d4e99adf06996b0cb0e2593e2f6
Ombly <patrascu.marian@hotmail.com>
2024-03-13 21:09:11 +0200

    wb

---
+++ cvtemplate/src/index.js
@@ -0,0 +1,13 @@
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import particles from 'particles.js'
+import './js/particles.js'
+
+
+import '../node_modules/typed.js/dist/typed.umd.js'
+
+import './js/main.js'
