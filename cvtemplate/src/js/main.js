d45a30142f3f5fe3ed1dd7cce1e7f6619f481b27
Ombly <patrascu.marian@hotmail.com>
2024-03-14 20:51:37 +0200

    curs2

--- cvtemplate/src/js/main.js
+++ cvtemplate/src/js/main.js
@@ -226,4 +226,23 @@
      */
     // new PureCounter();
 
+})();
+
+  /**
+     Validare formular
+     */
+(function () {
+    'use strict'
+    const forms = document.querySelectorAll('.requires-validation')
+    Array.from(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
 })()
